e8a0265344e532f09be7245837cd4ff908f3270b
Tarantil <isurdu3@gmail.com>
2026-07-08 11:29:41 +0200

    Added items to the navigation

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -5,9 +5,9 @@ export default function MainNavigation(){
         <header>
             <ul>
                 <li><NavLink to="/" className={({isActive})=>isActive?'active':undefined}>Dashboard</NavLink></li>
-                <li><NavLink to="/exercises" className={({isActive})=>isActive?'active':undefined}></NavLink></li>
-                <li><NavLink to="/progress" className={({isActive})=>isActive?'active':undefined}></NavLink></li>
-                <li><NavLink to="/workouts" className={({isActive})=>isActive?'active':undefined}></NavLink></li>
+                <li><NavLink to="/exercises" className={({isActive})=>isActive?'active':undefined}>Exercises</NavLink></li>
+                <li><NavLink to="/progress" className={({isActive})=>isActive?'active':undefined}>Progress</NavLink></li>
+                <li><NavLink to="/workouts" className={({isActive})=>isActive?'active':undefined}>Workouts</NavLink></li>
             </ul>
         </header>
     );
